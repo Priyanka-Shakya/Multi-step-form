526d8969b807050b3ced06f9decdacf82ad02fbb
Siya <priyankashakya1404@gmail.com>
2023-02-04 23:38:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+
+// document.querySelector(".rightcontainer").style.display = "none";
+document.querySelector(".thirdcontainer").style.display = "none";
+document.querySelector(".fourthcontainer").style.display = "none";
+document.querySelector(".fifthcontainer").style.display = "none";
+document.querySelector(".thanku").style.display = "none";
+
+function myfunction1(){
+document.querySelector(".rightcontainer").style.display = "none";
+document.getElementById("smile1").style.backgroundColor="lightgreen";
+document.querySelector(".thirdcontainer").style.display = "block";
+document.querySelector(".fourthcontainer").style.display = "none";
+document.querySelector(".fifthcontainer").style.display = "none";
+document.querySelector(".thanku").style.display = "none";
+}
+function myfunction2(){
+document.querySelector(".rightcontainer").style.display = "none";
+document.querySelector(".thirdcontainer").style.display = "none";
+document.getElementById("smile2").style.backgroundColor="lightgreen";
+document.querySelector(".fourthcontainer").style.display = "block";
+document.querySelector(".fifthcontainer").style.display = "none";
+document.querySelector(".thanku").style.display = "none";
+}
+function myfunction3(){
+document.querySelector(".rightcontainer").style.display = "none";
+document.querySelector(".thirdcontainer").style.display = "none";
+document.querySelector(".fourthcontainer").style.display = "none";
+document.getElementById("smile3").style.backgroundColor="lightgreen";
+document.querySelector(".fifthcontainer").style.display = "block";
+document.querySelector(".thanku").style.display = "none";
+}
+function myfunction4(){
+document.querySelector(".rightcontainer").style.display = "none";
+document.querySelector(".thirdcontainer").style.display = "none";
+document.querySelector(".fourthcontainer").style.display = "none";
+document.getElementById("smile4").style.backgroundColor="lightgreen";
+document.querySelector(".fifthcontainer").style.display = "none";
+document.querySelector(".maincontainer").style.display = "none";
+document.querySelector(".thanku").style.display = "block";
+}
+
+
+
+
